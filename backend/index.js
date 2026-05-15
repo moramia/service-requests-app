2ba97bfd417a1c4985bad291f667e7956dd5be89
Victoria Karapetyan <vika.serg.karap@gmail.com>
2026-05-15 23:51:39 +0300

    chore(backend): добавлен Dockerfile

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,7 @@ app.get("/", (req, res) => {
   res.send("API работает");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
